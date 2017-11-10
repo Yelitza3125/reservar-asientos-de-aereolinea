cb79f5842251599460ea2b4ccaf87baf8c506eef
Yelitza3125 <deyel_99@hotmail.com>
2017-11-10 01:45:44 -0500

    Iterando el array seats

--- js/app.js
+++ js/app.js
@@ -25,6 +25,16 @@ var paintSeats = function(array) {
  for(var i= 0; i< array.length; i++ ) {
      var seat = document.createElement('div');
      seat.className = 'seats';
- }
-}
 
+     //del primer elemento al cuarto es Primera Clase. De i = 0 a i = 3
+     if(i < 4) {
+         seat.style.background = 'purple';
+     } else {
+         seat.style.background = 'yellow';
+     }
+     containerSeat.appendChild(seat);
+ }
+
+};
+
+paintSeats(airlineSeats);
